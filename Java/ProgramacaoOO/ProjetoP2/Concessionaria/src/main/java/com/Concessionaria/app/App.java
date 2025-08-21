b1f8d86ab8f3bdb6527137bc0b2ea19860f348a5
Rafael Menezes <109706910+RGMenezes@users.noreply.github.com>
2025-08-20 21:04:06 -0300

    delete project p2

--- Java/ProgramacaoOO/ProjetoP2/Concessionaria/src/main/java/com/Concessionaria/app/App.java
+++
@@ -1,7 +0,0 @@
-// package Java.ProgramacaoOO.ProjetoP2.Concessionaria.src.main.java.com.Concessionaria.app;
-
-public class App {
-    public static void main( String[] args ){
-        System.out.println( "Hello World!" );
-    }
-}
